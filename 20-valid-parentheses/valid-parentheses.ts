2f372576f440c952f3015cbb6ccddeaf7c42e85e
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-01-14 17:57:22 +0900

    Time: 338 ms (5.08%) | Memory: 52.1 MB (5.03%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.ts
@@ -0,0 +1,16 @@
+function isValid(s: string): boolean {
+    const map = {
+        ")":"(",
+        "}":"{",
+        "]":"[",
+    }
+    const stack = [];
+
+    for(const i of s){
+        console.log(i,stack,map[i])
+        if(["(","[","{"].includes(i))stack.push(i)
+        else if(stack.pop() !== map[i])return false   
+    }
+
+    return !stack.length;
+};
