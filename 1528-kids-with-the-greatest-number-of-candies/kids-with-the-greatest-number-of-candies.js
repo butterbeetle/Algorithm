a35ece4d25528629a23a40092184ac902a7b4a70
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-01-16 14:18:55 +0900

    Time: 55 ms (60.28%) | Memory: 42.1 MB (80.41%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    return candies.map((candy)=>candy+extraCandies >= Math.max(...candies))
+};
