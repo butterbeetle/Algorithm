21652fb2724a51f0f992bdc4ea28770b6c91e1bc
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-01-18 01:03:25 +0900

    Time: 62 ms (15.66%) | Memory: 41.9 MB (46.51%) - LeetSync

--- 66-plus-one/plus-one.js
+++ 66-plus-one/plus-one.js
@@ -3,8 +3,10 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
+    // 48ms
     digits.unshift(0)
     digits[digits.length - 1] += 1;
+
     for(let i=digits.length-1; i>=0; i--){
         if(digits[i] === 10){
             digits[i] = 0;
@@ -13,6 +15,7 @@ var plusOne = function(digits) {
     }
 
     return !digits[0] ? digits.splice(1) : digits
+
     // 49ms
     //return (BigInt(digits.join(""))+1n).toString().split("")
 };
