202495a1d86b53e702b8b55387a0d77218b752d9
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-01-18 20:33:15 +0900

    Time: 66 ms (51.45%) | Memory: 48.6 MB (10.77%) - LeetSync

---
+++ 136-single-number/single-number.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    const map = new Map()
+    for(const i of nums){
+        map.set(i, (map.get(i)||0)+1)
+    }
+
+    for(const [k,v] of map){
+        if(v === 1) return k
+    }
+};
