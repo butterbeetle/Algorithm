567fc30340dd1bc158babecc8753a768e58fb717
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-01-19 21:38:44 +0900

    Time: 153 ms (32.70%) | Memory: 72.8 MB (7.96%) - LeetSync

--- 1742-widest-vertical-area-between-two-points-containing-no-points/widest-vertical-area-between-two-points-containing-no-points.js
+++ 1742-widest-vertical-area-between-two-points-containing-no-points/widest-vertical-area-between-two-points-containing-no-points.js
@@ -4,15 +4,11 @@
  */
 var maxWidthOfVerticalArea = function(points) {
     const arr = points.sort((a,b)=>a[0]-b[0]).map(n=>n[0]);
-    let p = arr[0];
     let area = 0;
 
-    for(let i=1; i<arr.length; i++){
-        let tmp = 0;
-        if(p < arr[i]) {
-            tmp = arr[i] - p
-            area = tmp > area ? tmp : area
-            p = arr[i]
+    for(let i=0; i<arr.length-1; i++){
+        if(arr[i+1] - arr[i] > area ) {
+            area = arr[i+1] - arr[i]
         }
     }
 
