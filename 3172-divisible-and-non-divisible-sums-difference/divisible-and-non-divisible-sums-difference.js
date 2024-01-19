f48cb78a967f44fef8585a0017a27f3195e1a05e
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-01-20 01:18:29 +0900

    Time: 64 ms (33.62%) | Memory: 53.1 MB (5.77%) - LeetSync

---
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @param {number} m
+ * @return {number}
+ */
+var differenceOfSums = function(n, m) {
+    const [nd,d] = Array.from({length:n},(_,i)=>i+1).reduce(([nd,d],cur)=>{
+        if(cur%m) return [nd+cur,d]
+        else return [nd,d+cur]
+    },[0,0])
+
+    return nd - d 
+};
