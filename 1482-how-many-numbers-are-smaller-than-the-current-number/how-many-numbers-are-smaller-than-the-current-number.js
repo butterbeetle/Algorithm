badd4cd7dc7fd09f40e83206cbbebac8951cdc46
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-01-20 17:37:37 +0900

    Time: 78 ms (30.57%) | Memory: 55.3 MB (5.37%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.js
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.js
@@ -3,5 +3,15 @@
  * @return {number[]}
  */
 var smallerNumbersThanCurrent = function(nums) {
+    // const hashmap = new Map();
+    // const array = [...nums].sort((a,b) => a - b);
+    // for (let i = 0; i < array.length; i++) {
+    //     if (!hashmap.has(array[i])) hashmap.set(array[i], i);
+    // }
+    // console.log(hashmap)
+    // for (let i = 0; i < nums.length; i++) {
+    //     nums[i] = hashmap.get(nums[i]);
+    // }
+    // return nums;
     return nums.map(m=> nums.filter(f=>m>f).length)
 };
