12bf9626c7169b13a921eaf3d7a07c7e8de71a6a
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-01-21 13:15:24 +0900

    Time: 94 ms (15.55%) | Memory: 57 MB (6.40%) - LeetSync

---
+++ 1839-decode-xored-array/decode-xored-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} encoded
+ * @param {number} first
+ * @return {number[]}
+ */
+var decode = function(encoded, first) {
+    // [4,6,2,7,3]
+    console.log(0^4^6, 0^4^6^2, 0^4^6^2^7, 0^4^6^2^7^3)
+    const arr = [first]
+
+    for(let i=0; i<encoded.length; i++){
+        arr[i+1] = arr[i]^encoded[i]
+    }
+    
+    return arr
+};
