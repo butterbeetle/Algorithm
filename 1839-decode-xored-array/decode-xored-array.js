8869e7f71d16aadd0fc29f769b1521ef9fd85293
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-01-21 13:16:00 +0900

    Time: 89 ms (28.35%) | Memory: 57.1 MB (6.40%) - LeetSync

--- 1839-decode-xored-array/decode-xored-array.js
+++ 1839-decode-xored-array/decode-xored-array.js
@@ -9,7 +9,7 @@ var decode = function(encoded, first) {
     const arr = [first]
 
     for(let i=0; i<encoded.length; i++){
-        arr[i+1] = arr[i]^encoded[i]
+        arr.push(arr[i]^encoded[i])
     }
     
     return arr
