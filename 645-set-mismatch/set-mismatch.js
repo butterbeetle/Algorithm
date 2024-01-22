538b7fe58fe96d12c69cd1ffc1baa4882b8b27b7
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-01-22 12:29:35 +0900

    Time: 88 ms (29.18%) | Memory: 61.6 MB (5.25%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.js
@@ -0,0 +1,19 @@
+/** 00:01:45
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findErrorNums = function(nums) {
+    const map = new Map()
+    const arr = [0,0]
+
+    for(const i in nums){
+        map.set(nums[i], (map.get(nums[i])||0)+1)
+    }
+    console.log(map)
+    for(const i in nums){
+        if(map.get(+i+1) > 1) arr[0] = +i+1
+        else if(map.get(+i+1) === undefined) arr[1] = +i+1
+    }
+
+    return arr
+};
