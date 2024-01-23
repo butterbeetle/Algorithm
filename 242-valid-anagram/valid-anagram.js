809930e17229524343d52653038bc5d3d877cb3a
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-01-23 14:45:51 +0900

    Time: 61 ms (87.87%) | Memory: 51.1 MB (5.06%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -5,20 +5,17 @@
  */
 var isAnagram = function(s, t) {
     if(s.length !== t.length) return false
-    
-    const map1 = new Map();
-    const map2 = new Map();
+
+    const map = new Map();
 
     for(let i=0; i<s.length; i++){
-        map1.set(s[i], (map1.get(s[i])||0)+1)
-        map2.set(t[i], (map2.get(t[i])||0)+1)
+        map.set(s[i], (map.get(s[i])||0)+1)
+        map.set(t[i], (map.get(t[i])||0)-1)
     }
 
-    console.log(map1, map2)
-
-    for(const [key,value] of map1){
-        if(map2.get(key) !== value) return false
+    for(const v of map.values()){
+        if(v) return false
     }
-
+    
     return true
 };
