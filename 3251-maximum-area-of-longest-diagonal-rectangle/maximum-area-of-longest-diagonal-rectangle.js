8ed57e4ead55ba453e89266c80e32ad518a74378
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-01-24 18:38:04 +0900

    Time: 61 ms (67.97%) | Memory: 51.5 MB (5.54%) - LeetSync

---
+++ 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} dimensions
+ * @return {number}
+ */
+var areaOfMaxDiagonal = function(dimensions) {
+    let max = 0;
+    let output = 0;
+    for(const i in dimensions){
+        const [len, wid] = dimensions[i]
+        const Diagonal = Math.sqrt((len**2)+(wid**2))
+        if(max < Diagonal){
+            max = Diagonal;
+            output = (len * wid);
+        }else if(max === Diagonal){
+            output = output < (len*wid) ? (len*wid) : output
+        }
+        
+    }
+    return output
+};
