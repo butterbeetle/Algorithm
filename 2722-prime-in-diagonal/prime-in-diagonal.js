ee5277db0f1ae2477787fdaa7d74d8bb019050f7
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-01-24 19:31:17 +0900

    Time: 77 ms (39.52%) | Memory: 56.9 MB (10.48%) - LeetSync

---
+++ 2722-prime-in-diagonal/prime-in-diagonal.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[][]} nums
+ * @return {number}
+ */
+var diagonalPrime = function(nums) {
+    let output = 0;
+
+    const isPrime = (n) => {
+        if(n<2) return false
+
+        for(let i=2; i<=Math.sqrt(n); i++){
+            if (!(n%i)) return false
+        }
+
+        return true
+    }
+
+    for(let i=0, j=nums.length-1; i<nums.length; i++, j--){
+        if(isPrime(nums[i][i])) output = Math.max(output, nums[i][i])
+        if(isPrime(nums[i][j])) output = Math.max(output, nums[i][j])
+    }
+
+    return output
+};
