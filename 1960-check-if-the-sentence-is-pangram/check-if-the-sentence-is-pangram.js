ed8ede000d2d64ffe4dbdf185230654bcd4b5609
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-01-25 22:14:43 +0900

    Time: 50 ms (77.49%) | Memory: 50.3 MB (5.11%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} sentence
+ * @return {boolean}
+ */
+var checkIfPangram = function(sentence) {
+    const set = new Set();
+
+    for(const s of sentence){
+        set.add(s)
+    }
+
+    return set.size === 26 
+};
