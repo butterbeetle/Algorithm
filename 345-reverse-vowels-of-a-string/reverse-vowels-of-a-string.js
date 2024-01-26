a6f3919cff091d4ee4bd9ff3b5912960a79dbacb
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-01-26 16:34:16 +0900

    Time: 67 ms (71.40%) | Memory: 56.2 MB (11.65%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.js
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.js
@@ -16,10 +16,8 @@ var reverseVowels = function (s) {
         if(isVowel(s[i])) arr.push(s[i])
     }
 
-    const reverse = arr.reverse();
-    
     for (let i = 0; i < s.length; i++) {
-        if(isVowel(s[i])) str += reverse.shift()
+        if(isVowel(s[i])) str += arr.pop()
         else str += s[i]
     }
 
