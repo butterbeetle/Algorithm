708cdcc1e7a915f44f7daeff88cbfc6f385c0807
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-01-28 01:02:39 +0900

    Time: 44 ms (90.80%) | Memory: 48.9 MB (6.98%) - LeetSync

--- 228-summary-ranges/summary-ranges.js
+++ 228-summary-ranges/summary-ranges.js
@@ -4,22 +4,18 @@
  */
 var summaryRanges = function(nums) {
     const output = [];
-    if(!nums.length) return output
     let s = nums[0]
     let e = s
     
-    for(let i=1; i<nums.length; i++){
-        if(nums[i]-nums[i-1] === 1) e = nums[i]
+    for(let i=0; i<nums.length; i++){
+        if(nums[i+1]-nums[i] === 1) e = nums[i+1]
         else{ 
             if(s === e) output.push(`${s}`)
             else output.push(`${s}->${e}`)
-            s = nums[i]
+            s = nums[i+1]
             e = s
         }
     }
-    
-    if(s === e) output.push(`${s}`)
-    else output.push(`${s}->${e}`)
 
     return output
 };
