270842eb302a05c027f21eccc7407641f98ea992
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-01-28 00:43:10 +0900

    Time: 79 ms (86.30%) | Memory: 73.9 MB (12.35%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.js
+++ 219-contains-duplicate-ii/contains-duplicate-ii.js
@@ -4,10 +4,11 @@
  * @return {boolean}
  */
 var containsNearbyDuplicate = function(nums, k) {
+    const map = new Map();
+
     for(let i=0; i<nums.length; i++){
-        for(let j=i+1; j<=i+k && j<nums.length; j++){
-            if(nums[i] === nums[j]) return true
-        }
+        if(map.has(nums[i]) && ( i-map.get(nums[i]) )<= k) return true
+        map.set(nums[i], i)
     }
 
     return false
