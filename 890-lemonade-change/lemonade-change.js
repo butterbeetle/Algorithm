994b01250a8a85ce30d2f35bdeeccab4c98a2519
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-01-31 21:03:40 +0900

    Time: 371 ms (5.85%) | Memory: 68.2 MB (5.26%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} bills
+ * @return {boolean}
+ */
+var lemonadeChange = function (bills) {
+    const map = new Map();
+
+    for (const bill of bills) {
+        map.set(bill, (map.get(bill) || 0) + 1)
+
+        if (bill === 10) {
+            map.set(5, (map.get(5) || 0) - 1)
+        } else if (bill === 20) {
+            if (map.get(10) > 0 && map.get(5) > 0) {
+                map.set(10, (map.get(10) || 0) - 1)
+                map.set(5, (map.get(5) || 0) - 1)
+            }
+            else map.set(5, (map.get(5) || 0) - 3)
+        }
+
+        if (map.get(5) < 0 || map.get(10) < 0) return false
+        console.log(bill, map)
+    }
+
+    for (const v of map.values()) {
+        if (v < 0) return false
+    }
+    console.log(map)
+    return true
+};
+
+// 15 => 5 3개 or 10 1개 5 1개
