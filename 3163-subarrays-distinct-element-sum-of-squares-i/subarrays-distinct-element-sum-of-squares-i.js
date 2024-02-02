4e794a5a967504474e24393e8fb962ce0d575b20
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-02-02 23:30:40 +0900

    Time: 199 ms (33.65%) | Memory: 55.3 MB (15.38%) - LeetSync

--- 3163-subarrays-distinct-element-sum-of-squares-i/subarrays-distinct-element-sum-of-squares-i.js
+++ 3163-subarrays-distinct-element-sum-of-squares-i/subarrays-distinct-element-sum-of-squares-i.js
@@ -1,15 +1,18 @@
-/**
+/** 00:16:21
  * @param {number[]} nums
  * @return {number}
  */
-var sumCounts = function(nums) {
+var sumCounts = function (nums) {
+    const nLen = nums.length
     let output = 0;
-    for(let i=0; i<nums.length; i++){
+
+    for (let i = 0; i < nLen; i++) {
         let sum = 0;
-        for(let j=0; j<nums.length-i; j++){
-            sum += new Set(nums.slice(j,j+i+1)).size ** 2
+        for (let j = 0; j < nLen - i; j++) {
+            sum += new Set(nums.slice(j, j + i + 1)).size ** 2
         }
         output += sum
     }
+
     return output
 };
