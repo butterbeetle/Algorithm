bc014db1ffa01ba562d80ac224492a2fc9f85e51
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-02-05 16:32:52 +0900

    Time: 73 ms (73.32%) | Memory: 53.2 MB (47.23%) - LeetSync

--- 2844-sum-of-squares-of-special-elements/sum-of-squares-of-special-elements.js
+++ 2844-sum-of-squares-of-special-elements/sum-of-squares-of-special-elements.js
@@ -4,10 +4,9 @@
  */
 var sumOfSquares = function (nums) {
     let output = 0;
-    const n = nums.length;
 
     for (const i in nums) {
-        if (!(n % (+i+1))) output += nums[i] ** 2
+        if (!(nums.length % (+i+1))) output += nums[i] ** 2
     }
 
     return output
