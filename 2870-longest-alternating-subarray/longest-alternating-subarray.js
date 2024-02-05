ef37d09179545b947f694574e23ec2d153fe4b5e
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-02-05 16:23:27 +0900

    Time: 216 ms (5.71%) | Memory: 57.7 MB (6.67%) - LeetSync

--- 2870-longest-alternating-subarray/longest-alternating-subarray.js
+++ 2870-longest-alternating-subarray/longest-alternating-subarray.js
@@ -1,25 +1,26 @@
-/** 00:29:45
+/** 00:29:45 (207ms)
  * @param {number[]} nums
  * @return {number}
  */
 var alternatingSubarray = function (nums) {
-    let l = 2;
+    let l = 1;
     let output = -1;
 
-    while (l <= nums.length) {
+    while (l++ <= nums.length) {
         for (let i = l; i <= nums.length; i++) {
             let noop = false
             const slice_arr = nums.slice(i - l, i);
             const len = slice_arr.length
 
             for (let j = 1; j < len; j++) {
-                if (slice_arr[j] - slice_arr[j - 1] !== Math.pow(-1, j + 1)) noop = true
-                if (noop) break;
+                if (slice_arr[j] - slice_arr[j - 1] !== Math.pow(-1, j + 1)) {
+                    noop = !noop
+                    break;
+                }
             }
-            
+
             if (!noop) output = Math.max(output, len)
         }
-        l++;
     }
 
     return output
