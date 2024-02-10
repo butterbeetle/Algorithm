1b780a4de89235712c904028a8e72dd421945ef1
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-02-10 22:46:59 +0900

    Time: 62 ms (39.31%) | Memory: 49.5 MB (24.01%) - LeetSync

---
+++ 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingInteger = function (nums) {
+    let output = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        output += nums[i]
+        if (nums[i] + 1 !== nums[i + 1]) break;
+    }
+
+    if (!nums.includes(output)) return output;
+
+    while (nums.includes(output)) {
+        output += 1
+    }
+
+    return output
+};
