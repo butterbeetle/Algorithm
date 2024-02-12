9ca664f154f97298d9ae2e6dae44d086f5c9d1d1
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-02-12 15:32:11 +0900

    Time: 155 ms (76.93%) | Memory: 58.7 MB (26.47%) - LeetSync

--- 326-power-of-three/power-of-three.js
+++ 326-power-of-three/power-of-three.js
@@ -2,8 +2,8 @@
  * @param {number} n
  * @return {boolean}
  */
-var isPowerOfThree = function(n) {
-    if(n < 1 || n%1) return false
-    if(n === 1) return true
-    return isPowerOfThree(n/3)
+var isPowerOfThree = function (n) {
+    if (n < 1 || n % 1) return false
+    if (n === 1) return true
+    return isPowerOfThree(n / 3)
 };
