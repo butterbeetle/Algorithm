fc164b1a8d0ec1bc504b7d51b94328cf81c85b1b
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-02-12 16:05:50 +0900

    Time: 1906 ms (5.08%) | Memory: 49 MB (14.65%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.js
+++ 367-valid-perfect-square/valid-perfect-square.js
@@ -3,5 +3,12 @@
  * @return {boolean}
  */
 var isPerfectSquare = function (num) {
-    return !(Math.sqrt(num) % 1)
+    // return !(Math.sqrt(num) % 1)
+    if(num === 1) return true
+
+    for (let i = 1; i <= num / 2; i++) {
+        if (i ** 2 === num) return true
+    }
+    
+    return false
 };
