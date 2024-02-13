b04d7d0c1ea36e6b247593552d9592e6a7cfaa23
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-02-13 11:01:00 +0900

    Time: 97 ms (7.22%) | Memory: 55.5 MB (5.25%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.js
+++ 412-fizz-buzz/fizz-buzz.js
@@ -6,10 +6,10 @@ var fizzBuzz = function (n) {
     let output = [];
 
     for (let i = 1; i <= n; i++) {
-        if (!(i % 3) && !(i % 5)) output = [...output, "FizzBuzz"]
+        if (!(i % 15)) output = [...output, "FizzBuzz"]
         else if (!(i % 3)) output = [...output, "Fizz"]
         else if (!(i % 5)) output = [...output, "Buzz"]
-        else output = [...output, i.toString()]
+        else output = [...output, `${i}`]
     }
 
     return output
