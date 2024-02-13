fec8ec585929d1ac25b0104ae92e92a46b438548
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-02-13 15:01:43 +0900

    Time: 1100 ms (61.34%) | Memory: 49.5 MB (6.70%) - LeetSync

---
+++ 507-perfect-number/perfect-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var checkPerfectNumber = function (num) {
+    let sum = 0;
+    for (let i = 1; i <= num / 2; i++) {
+        if (!(num % i)) sum += i;
+    }
+    return sum === num
+};
