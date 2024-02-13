76d295b20d56703be42310abf6f6d307200d775b
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-02-13 14:04:47 +0900

    Time: 69 ms (25.57%) | Memory: 50.9 MB (22.85%) - LeetSync

--- 728-self-dividing-numbers/self-dividing-numbers.js
+++ 728-self-dividing-numbers/self-dividing-numbers.js
@@ -9,8 +9,8 @@ var selfDividingNumbers = function (left, right) {
     for (let i = left; i <= right; i++) {
         let isDividingNumber = true
 
-        for (const n of i + "") {
-            if (!n || (i % n) != 0) isDividingNumber = false
+        for (const d of i + "") {
+            if (!d || (i % d) != 0) isDividingNumber = false
         }
 
         if (isDividingNumber) output.push(i)
