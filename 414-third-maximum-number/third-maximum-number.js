f8d331f78a489664271284889b8319097a5ae699
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-02-13 11:37:44 +0900

    Time: 50 ms (85.38%) | Memory: 50.9 MB (26.58%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.js
+++ 414-third-maximum-number/third-maximum-number.js
@@ -3,7 +3,11 @@
  * @return {number}
  */
 var thirdMax = function (nums) {
+    const n = nums.length
+    if (n === 1) return nums[0]
+    if (n === 2) return Math.max(nums[0], nums[1])
+
     const arr = Array.from(new Set(nums)).sort((a, b) => b - a)
-    console.log(arr)
+
     return arr[2] ?? arr[0]
 };
