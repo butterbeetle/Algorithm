df625021f3f961329071f681e3bc6fbd15031fcf
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-02-20 16:16:30 +0900

    Time: 49 ms (99.09%) | Memory: 49 MB (28.18%) - LeetSync

---
+++ 1656-count-good-triplets/count-good-triplets.js
@@ -0,0 +1,22 @@
+/** 00:04:20
+ * @param {number[]} arr
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
+ */
+var countGoodTriplets = function (arr, a, b, c) {
+    let output = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (Math.abs(arr[i] - arr[j]) > a) continue
+            for (let k = j + 1; k < arr.length; k++) {
+                if (Math.abs(arr[j] - arr[k]) > b || Math.abs(arr[i] - arr[k]) > c) continue
+                output++;
+            }
+        }
+    }
+
+    return output
+};
