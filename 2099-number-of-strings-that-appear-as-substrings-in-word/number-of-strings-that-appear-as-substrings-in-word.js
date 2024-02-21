ade9f513aa59c92ff5bb7ceb4ab8d12ed278da39
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-02-21 15:13:34 +0900

    Time: 50 ms (77.29%) | Memory: 49 MB (44.48%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.js
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.js
@@ -7,7 +7,7 @@ var numOfStrings = function (patterns, word) {
     let output = 0;
 
     for (let i = 0; i < patterns.length; i++) {
-        if (word.indexOf(patterns[i]) >= 0) output++;
+        if (word.includes(patterns[i])) output++;
     }
 
     return output;
