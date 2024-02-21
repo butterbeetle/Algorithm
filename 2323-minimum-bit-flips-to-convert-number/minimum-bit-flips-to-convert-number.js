996c3f5469da52c512ec35fcfddb3c4b337166ba
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-02-21 16:06:30 +0900

    Time: 60 ms (24.11%) | Memory: 48.2 MB (45.54%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.js
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.js
@@ -1,22 +1,8 @@
-/**
+/** 00:04:32
  * @param {number} start
  * @param {number} goal
  * @return {number}
  */
 var minBitFlips = function (start, goal) {
-    let output = 0;
-    let startBinary = start.toString(2);
-    let goalBinary = goal.toString(2);
-
-    const len = startBinary.length > goalBinary.length ? startBinary.length : goalBinary.length
-
-    startBinary = startBinary.padStart(len, "0")
-    goalBinary = goalBinary.padStart(len, "0")
-
-    for (let i = 0; i < startBinary.length; i++) {
-        console.log(startBinary[i], goalBinary[i])
-        if (startBinary[i] !== goalBinary[i]) output++;
-    }
-
-    return output
+    return (start ^ goal).toString(2).split("0").join("").length
 };
