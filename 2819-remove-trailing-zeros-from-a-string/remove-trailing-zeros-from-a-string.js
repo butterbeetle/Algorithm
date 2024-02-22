c45589f1e9a1df46624b30a129345dfdadd3f9b9
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-02-22 16:08:38 +0900

    Time: 65 ms (59.02%) | Memory: 51.4 MB (31.01%) - LeetSync

---
+++ 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var removeTrailingZeros = function (num) {
+    let idx = 0;
+
+    for (let i = num.length - 1; i >= 0; i--) {
+        if (num[i] != 0) {
+            idx = i;
+            break;
+        }
+    }
+
+    return num.substring(0, idx + 1)
+};
