ee2b71815fc4e97877708fcb4eb3b8da5d4ff663
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-02-26 17:40:15 +0900

    Time: 224 ms (6.14%) | Memory: 66.1 MB (5.03%) - LeetSync

---
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.js
@@ -0,0 +1,20 @@
+/** 00:15:49
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var minSteps = function (s, t) {
+    let output = 0;
+    const m1 = new Map();
+
+    for (const i in s) {
+        m1.set(s[i], (m1.get(s[i]) || 0) + 1)
+        m1.set(t[i], (m1.get(t[i]) || 0) - 1)
+    }
+
+    for (const v of m1.values()) {
+        if (v > 0) output += v;
+    }
+
+    return output
+};
