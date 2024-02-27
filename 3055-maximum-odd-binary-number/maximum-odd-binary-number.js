5da942cdf863637a35cf751422aa19733e59147c
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-02-27 17:57:18 +0900

    Time: 77 ms (50.60%) | Memory: 53.1 MB (23.61%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.js
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.js
@@ -1,29 +1,15 @@
-/** 00:06:29
+/** 00:07:29
  * @param {string} s
  * @return {string}
  */
 var maximumOddBinaryNumber = function (s) {
-    let one = 0;
+    let one = -1;
     let zero = 0;
-    let output = "";
 
     for (const c of s) {
         if (c == 1) one++;
         else zero++;
     }
 
-    one--;
-
-    while (one + zero > 0) {
-        if (one) {
-            output = output + "1"
-            one--
-        }
-        else {
-            output = output + "0"
-            zero--
-        }
-    }
-
-    return output + "1"
+    return "1".repeat(one) + "0".repeat(zero) + "1"
 };
