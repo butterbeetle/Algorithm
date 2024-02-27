3301eb0390eef53dc9bc939d5210776afa83d256
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-02-27 17:32:14 +0900

    Time: 59 ms (27.10%) | Memory: 49.6 MB (12.15%) - LeetSync

--- 2088-minimum-time-to-type-word-using-special-typewriter/minimum-time-to-type-word-using-special-typewriter.js
+++ 2088-minimum-time-to-type-word-using-special-typewriter/minimum-time-to-type-word-using-special-typewriter.js
@@ -1,23 +1,19 @@
-/**
+/** 00:15:57
  * @param {string} word
  * @return {number}
  */
 var minTimeToType = function (word) {
     let output = 0;
     let point = "a";
+
     const map = "abcdefghijklmnopqrstuvwxyz".split("").reduce((acc, cur, i) => {
         acc.set(cur, i + 1);
         return acc
     }, new Map());
 
     for (const w of word) {
-        let x = map.get(w) - map.get(point);
-        x = x < 0 ? x + 26 : x
-        let y = map.get(point) - map.get(w)
-        y = y < 0 ? y + 26 : y
-
-        if (point !== w) output += Math.min(x, y)
-        output += 1;
+        const dist = Math.abs(map.get(point) - map.get(w))
+        output += Math.min(dist, 26 - dist) + 1
         point = w;
     }
 
