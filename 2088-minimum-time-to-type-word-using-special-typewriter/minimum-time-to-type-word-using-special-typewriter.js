5f7d141c39ac1581e6b7ff0b7063e5bb513ccc4a
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-02-27 17:29:35 +0900

    Time: 57 ms (41.12%) | Memory: 50.2 MB (5.61%) - LeetSync

--- 2088-minimum-time-to-type-word-using-special-typewriter/minimum-time-to-type-word-using-special-typewriter.js
+++ 2088-minimum-time-to-type-word-using-special-typewriter/minimum-time-to-type-word-using-special-typewriter.js
@@ -12,19 +12,13 @@ var minTimeToType = function (word) {
 
     for (const w of word) {
         let x = map.get(w) - map.get(point);
-        x = x < 0 ? x+26:x
+        x = x < 0 ? x + 26 : x
         let y = map.get(point) - map.get(w)
-        y = y < 0 ? y+26:y
-        console.log(`현재위치:${point} 가야할곳:${w}`)
-        if (point !== w){ 
-            console.log(`시계방향:${x}..반시계방향:${y}`)
-            output += Math.min(x, y)
-            console.log(`이동 후:${output}`)
-        }
+        y = y < 0 ? y + 26 : y
+
+        if (point !== w) output += Math.min(x, y)
         output += 1;
-        console.log(`누르기 후:${output}`)
         point = w;
-        console.log(`--------------------------`)
     }
 
     return output
