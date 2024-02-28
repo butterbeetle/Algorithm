f7a853e24b7f146b5ac9e5f4e89ca199b6181005
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-02-28 16:52:58 +0900

    Time: 62 ms (64.11%) | Memory: 52.8 MB (50.52%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.js
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.js
@@ -4,7 +4,7 @@
  */
 var largestOddNumber = function (num) {
     for (let i = num.length - 1; i >= 0; i--) {
-        if (num[i] % 2) return num.slice(0, i + 1)
+        if (num[i] % 2) return num.substring(0, i + 1)
     }
     return ""
 };
