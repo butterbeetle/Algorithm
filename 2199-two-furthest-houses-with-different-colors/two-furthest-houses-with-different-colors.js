e7fe4e46e567127bb52bea32ffa7196aff2ab166
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-02-28 16:01:17 +0900

    Time: 57 ms (39.44%) | Memory: 49.2 MB (14.79%) - LeetSync

--- 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.js
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.js
@@ -3,15 +3,10 @@
  * @return {number}
  */
 var maxDistance = function (colors) {
-    let s = 0;
-    
     for (let i = colors.length - 1; i >= 0; i--) {
-        if (colors[s] !== colors[i]) {
-            const d1 = colors.lastIndexOf(colors[i])
-            const d2 = Math.abs(colors.indexOf(colors[i]) - colors.lastIndexOf(colors[s]))
-            return Math.max(d1, d2)
+        if (colors[0] !== colors[i]) {
+            const d = Math.abs(colors.indexOf(colors[i]) - colors.lastIndexOf(colors[0]))
+            return Math.max(i, d)
         }
     }
-
-    return s
 };
