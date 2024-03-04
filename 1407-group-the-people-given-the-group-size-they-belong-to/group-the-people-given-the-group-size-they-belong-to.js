96f3e6008d97e9f3f4287762f931bec8d89a2710
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-03-04 16:54:44 +0900

    Time: 95 ms (23.74%) | Memory: 57.1 MB (24.37%) - LeetSync

--- 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.js
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.js
@@ -4,22 +4,39 @@
  */
 var groupThePeople = function (groupSizes) {
     const output = [];
-    const map = new Map();
+    const map = {};
 
     for (const i in groupSizes) {
-        const arr = map.get(groupSizes[i]) || [];
-        map.set(groupSizes[i], [...arr, i])
-    }
-
-    for (const [k, v] of map) {
-        while (v.length) {
-            const tmp = [];
-            for (let i = 0; i < k; i++) {
-                tmp.push(v.pop())
-            }
-            output.push(tmp)
+        const size = groupSizes[i]
+        if (!map[size]) map[size] = [];
+        map[size].push(i)
+        
+        if (map[size].length === size) {
+            output.push(map[size]);
+            delete map[size]
         }
     }
 
     return output
 };
+// var groupThePeople = function (groupSizes) {
+//     const output = [];
+//     const map = new Map();
+
+//     for (const i in groupSizes) {
+//         const arr = map.get(groupSizes[i]) || [];
+//         map.set(groupSizes[i], [...arr, i])
+//     }
+
+//     for (const [k, v] of map) {
+//         while (v.length) {
+//             const tmp = [];
+//             for (let i = 0; i < k; i++) {
+//                 tmp.push(v.pop())
+//             }
+//             output.push(tmp)
+//         }
+//     }
+
+//     return output
+// };
