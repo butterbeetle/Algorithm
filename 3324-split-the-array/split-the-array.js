38ed0ce004a572b84a70c952e036515b76f4ab83
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-03-05 18:09:50 +0900

    Time: 185 ms (5.18%) | Memory: 57.6 MB (5.57%) - LeetSync

---
+++ 3324-split-the-array/split-the-array.js
@@ -0,0 +1,15 @@
+/** 00:03:15
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isPossibleToSplit = function (nums) {
+    const map = new Map();
+
+    for (const num of nums) {
+        map.set(num, (map.get(num) || 0) + 1)
+        console.log(map)
+        if (map.get(num) > 2) return false
+    }
+
+    return true
+};
