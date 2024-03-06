f4848cb938660e46e60988527abf31aaf9c126a5
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-03-06 15:17:05 +0900

    Time: 60 ms (94.55%) | Memory: 52.1 MB (55.45%) - LeetSync

--- 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.js
+++ 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.js
@@ -1,36 +1,36 @@
-/**
+/** 00:36:51
  * @param {number[][]} mat
  * @return {number[][]}
  */
-var diagonalSort = function(mat) {
+var diagonalSort = function (mat) {
     const m = mat[0].length;
     const n = mat.length;
-    
-    const t = (i,j) => {
+
+    const jobs = (i, j) => {
         const arr = [];
         let row = i;
         let col = j;
 
-        while(mat[row] && mat[row][col]){
+        while (mat[row] && mat[row][col]) {
             arr.push(mat[row][col]);
             row++;
             col++;
         }
 
-        arr.sort((a,b)=>a-b)
+        arr.sort((a, b) => a - b)
 
-        while(arr.length){
+        while (arr.length) {
             --row;
             --col;
             mat[row][col] = arr.pop()
         }
     }
 
-    for(let col=0; col<m; col++){
-        t(0,col);
+    for (let col = 0; col < m; col++) {
+        jobs(0, col);
     }
-    for(let row=0; row<n; row++){
-        t(row,0);
+    for (let row = 0; row < n; row++) {
+        jobs(row, 0);
     }
 
     return mat
