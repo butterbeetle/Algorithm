0bd4baaef18be723885054d27a48601884a5aa51
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-03-06 17:12:30 +0900

    Time: 2741 ms (5.19%) | Memory: 63.4 MB (79.65%) - LeetSync

---
+++ 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.js
@@ -0,0 +1,15 @@
+/** 00:08:49
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minPairSum = function (nums) {
+    nums.sort((a, b) => a - b)
+    const len = nums.length / 2
+    let output = 0;
+    
+    for (let i = 0; i < len; i++) {
+        output = Math.max(output, nums.shift() + nums.pop())
+    }
+
+    return output
+};
