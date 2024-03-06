b7f0036b84e5705deda84993fb58bc400cd993fe
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-03-06 16:59:34 +0900

    Time: 106 ms (73.17%) | Memory: 53.5 MB (31.71%) - LeetSync

--- 2239-execution-of-all-suffix-instructions-staying-in-a-grid/execution-of-all-suffix-instructions-staying-in-a-grid.js
+++ 2239-execution-of-all-suffix-instructions-staying-in-a-grid/execution-of-all-suffix-instructions-staying-in-a-grid.js
@@ -1,4 +1,4 @@
-/** 00:31:44
+/** 00:36:44
  * @param {number} n
  * @param {number[]} startPos
  * @param {string} s
@@ -7,25 +7,19 @@
 var executeInstructions = function (n, startPos, s) {
     const output = [];
 
-    const move = (pos, oper) => {
-        const moveSet = {
-            "R": [0, 1],
-            "L": [0, -1],
-            "U": [-1, 0],
-            "D": [1, 0]
-        }
-
-        return [pos[0] + moveSet[oper][0], pos[1] + moveSet[oper][1]]
-    }
-
     for (let i = 0; i < s.length; i++) {
         let cnt = 0;
-        let tmp = [...startPos];
+        let [y, x] = [...startPos];
 
-        for (const char of s.slice(i)) {
-            const [x, y] = move(tmp, char);
-            if (x < 0 || y < 0 || x >= n || y >= n) break;
-            tmp = [x, y]
+        for (let j = i; j < s.length; j++) {
+            switch (s[j]) {
+                case 'L': x--; break;
+                case 'R': x++; break;
+                case 'U': y--; break;
+                case 'D': y++; break;
+            }
+
+            if (x < 0 || y < 0 || x === n || y === n) break;
             cnt++;
         }
 
