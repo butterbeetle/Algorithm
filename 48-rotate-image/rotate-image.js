c1e629f9a86c01669bf622415f55a85101f6dacf
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-03-08 16:26:01 +0900

    Time: 59 ms (22.65%) | Memory: 51.8 MB (6.61%) - LeetSync

---
+++ 48-rotate-image/rotate-image.js
@@ -0,0 +1,21 @@
+/** 00:25:47
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (mat) {
+    const n = mat.length;
+
+    // Transpose
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < i; j++) {
+            [mat[i][j], mat[j][i]] = [mat[j][i], mat[i][j]]
+        }
+    }
+
+    // Reverse
+    for (let i = 0; i < n; i++) {
+        for (let j = 0, k = n - 1; j < n / 2; j++, k--) {
+            [mat[i][j], mat[i][k]] = [mat[i][k], mat[i][j]]
+        }
+    }
+};
