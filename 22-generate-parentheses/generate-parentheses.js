407982d4f77344c1ebee3425a72593424fc1ed5a
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-03-08 15:54:26 +0900

    Time: 59 ms (30.09%) | Memory: 50.1 MB (64.35%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.js
+++ 22-generate-parentheses/generate-parentheses.js
@@ -1,21 +1,22 @@
-/** 0012:53
+/** 00:12:53
  * @param {number} n
  * @return {string[]}
  */
-var generateParenthesis = function(n) {
-    const output = [];
-    const t = (str,r,l) => {
-        if(r<l || r>n || l>n) return ;
-        if(r===n && l===n){
-            output.push(str);
+var generateParenthesis = function (n) {
+    const parentheses = [];
+    
+    const generate = (str, r, l) => {
+        if (r < l || r > n || l > n) return;
+        if (r === n && l === n) {
+            parentheses.push(str);
             return;
         }
 
-        t(str+"(",r+1,l)
-        t(str+")",r,l+1)
+        generate(str + "(", r + 1, l)
+        generate(str + ")", r, l + 1)
     }
 
-    t("(",1,0)
+    generate("(", 1, 0)
 
-    return output
+    return parentheses
 };
