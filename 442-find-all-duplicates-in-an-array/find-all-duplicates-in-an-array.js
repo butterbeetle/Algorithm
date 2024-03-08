33c36042712c4acec05db95389acef13f4981dd3
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-03-08 17:32:13 +0900

    Time: 84 ms (68.45%) | Memory: 56.8 MB (76.79%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.js
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.js
@@ -3,13 +3,25 @@
  * @return {number[]}
  */
 var findDuplicates = function (nums) {
-    const set = new Set();
-    const output = [];
+    let output = [];
 
-    for (const num of nums) {
-        if (set.has(num)) output.push(num);
-        set.add(num);
+    for (let i = 0; i < nums.length; i++) {
+        let position = Math.abs(nums[i]) - 1;
+        if (nums[position] < 0) output.push(Math.abs(nums[i]));
+        nums[position] *= -1;
     }
 
-    return output
+    return output;
 };
+
+// var findDuplicates = function (nums) {
+//     const set = new Set();
+//     const output = [];
+
+//     for (const num of nums) {
+//         if (set.has(num)) output.push(num);
+//         set.add(num);
+//     }
+
+//     return output
+// };
