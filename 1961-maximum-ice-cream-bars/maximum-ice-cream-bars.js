e45f311f2803d0bbfb9e963744bf51082ec4d241
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-03-08 17:11:14 +0900

    Time: 170 ms (90.00%) | Memory: 73.6 MB (5.56%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.js
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.js
@@ -4,15 +4,46 @@
  * @return {number}
  */
 var maxIceCream = function (costs, coins) {
-    costs.sort((a, b) => a - b)
     let output = 0;
+    const max = Math.max(...costs) + 1
+    const count_arr = new Array(max).fill(0);
+    const sorted_arr = [];
 
     for (const cost of costs) {
-        if (cost <= coins) {
-            coins -= cost;
+        count_arr[cost]++;
+    }
+    // console.log(count_arr);
+
+    for (let i = 0; i < count_arr.length; i++) {
+        count_arr[i] += count_arr[i - 1] ?? 0
+    }
+    // console.log(count_arr);
+
+    for (let i = costs.length - 1; i >= 0; i--) {
+        sorted_arr[count_arr[costs[i]]--] = costs[i];
+    }
+    sorted_arr.shift();
+    // console.log(sorted_arr)
+
+    for (const num of sorted_arr) {
+        if (num <= coins) {
+            coins -= num;
             output++;
         } else break;
     }
 
     return output
 };
+// var maxIceCream = function (costs, coins) {
+//     costs.sort((a, b) => a - b)
+//     let output = 0;
+
+//     for (const cost of costs) {
+//         if (cost <= coins) {
+//             coins -= cost;
+//             output++;
+//         } else break;
+//     }
+
+//     return output
+// };
