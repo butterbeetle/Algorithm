1550913a4a7bdc4ae74c0b09467a7457df902a86
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-03-08 18:08:18 +0900

    Time: 61 ms (55.81%) | Memory: 51.9 MB (24.42%) - LeetSync

--- 1652-minimum-suffix-flips/minimum-suffix-flips.js
+++ 1652-minimum-suffix-flips/minimum-suffix-flips.js
@@ -4,17 +4,30 @@
  */
 var minFlips = function(target) {
     let output = 0;
-    let s = "0".repeat(target.length);
-    
-    for(let i=0; i<target.length; i++){
-        if(target[i] !== s[i] && s[i] == 0){
+    let tmp = '0';
+
+    for (const t of target) {
+        if (t !== tmp) {
+            tmp = tmp == 0 ? "1":"0";
             output++;
-            s = s.substring(0,i) + "1".repeat(target.length-i);
-        }else if(target[i] !== s[i] && s[i] == 1){
-            output++;
-            s = s.substring(0,i) + "0".repeat(target.length-i);
         }
     }
 
-    return output;
-};
+    return output
+}
+// var minFlips = function(target) {
+//     let output = 0;
+//     let s = "0".repeat(target.length);
+    
+//     for(let i=0; i<target.length; i++){
+//         if(target[i] !== s[i] && s[i] == 0){
+//             output++;
+//             s = s.substring(0,i) + "1".repeat(target.length-i);
+//         }else if(target[i] !== s[i] && s[i] == 1){
+//             output++;
+//             s = s.substring(0,i) + "0".repeat(target.length-i);
+//         }
+//     }
+
+//     return output;
+// };
