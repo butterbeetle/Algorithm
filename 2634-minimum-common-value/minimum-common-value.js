015dc3aef6dda9e5177a0770676bf1f3434f7e2d
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-03-09 20:46:36 +0900

    Time: 85 ms (28.32%) | Memory: 69.5 MB (10.46%) - LeetSync

---
+++ 2634-minimum-common-value/minimum-common-value.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var getCommon = function (nums1, nums2) {
+    const set = new Set(nums1);
+
+    for (const num of nums2) {
+        if (set.has(num)) return num
+    }
+
+    return -1;
+};
