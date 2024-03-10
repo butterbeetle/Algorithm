90ae63f4d35a58d808d3b0e7e40b5f9d3829cdc0
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-03-10 20:49:22 +0900

    Time: 49 ms (68.97%) | Memory: 48.3 MB (77.40%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.js
+++ 54-spiral-matrix/spiral-matrix.js
@@ -1,31 +1,31 @@
-/**
+/** 00:21:36
  * @param {number[][]} matrix
  * @return {number[]}
  */
-var spiralOrder = function(matrix) {
+var spiralOrder = function (matrix) {
     const output = [];
     let m = -1;
-    let row = matrix.length-1;
+    let row = matrix.length - 1;
 
     let n = 0;
     let col = matrix[0].length;
 
     let inc = 1;
 
-    while(1){
-        for(let i=0; i<col; i++){
-            m+=inc;
+    while (1) {
+        for (let i = 0; i < col; i++) {
+            m += inc;
             output.push(matrix[n][m])
         }
         col--;
-        if(col<0) break;
+        if (col < 0) break;
 
-        for(let i=0; i<row; i++){
-            n+=inc;
+        for (let i = 0; i < row; i++) {
+            n += inc;
             output.push(matrix[n][m])
         }
         row--;
-        if(row<0) break;
+        if (row < 0) break;
 
         inc *= -1;
     }
