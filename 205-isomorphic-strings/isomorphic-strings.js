7f4c0e906ee9b0f67254212531a909fb417a75be
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-03-13 15:48:35 +0900

    Time: 57 ms (76.69%) | Memory: 50.7 MB (49.20%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+    const ms = new Map();
+    const mt = new Map();
+
+    for (let i = 0; i < t.length; i++) {
+        if (!ms.has(s[i])) ms.set(s[i], t[i])
+        else if (ms.get(s[i]) !== t[i]) return false;
+
+        if (!mt.has(t[i])) mt.set(t[i], s[i])
+        else if (mt.get(t[i]) !== s[i]) return false;
+    }
+
+    return true;
+};
