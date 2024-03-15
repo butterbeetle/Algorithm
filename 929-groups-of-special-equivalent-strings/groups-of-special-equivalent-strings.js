e3f4c98a24fde88f0fa85629898f0f00944f3010
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-03-15 16:37:07 +0900

    Time: 77 ms (7.69%) | Memory: 56.7 MB (7.69%) - LeetSync

---
+++ 929-groups-of-special-equivalent-strings/groups-of-special-equivalent-strings.js
@@ -0,0 +1,21 @@
+/** 00:28:51
+ * @param {string[]} words
+ * @return {number}
+ */
+var numSpecialEquivGroups = function (words) {
+    const set = new Set();
+
+    for (const word of words) {
+        const odd = [];
+        const even = [];
+
+        for (const i in word) {
+            if (!(i % 2)) even.push(word[i])
+            else odd.push(word[i])
+        }
+
+        set.add(odd.sort().join("") + even.sort().join(""))
+    }
+
+    return set.size
+};
