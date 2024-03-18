7f9e858d055b96faa9e48321ba53209998975ca3
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-03-18 15:49:59 +0900

    Time: 214 ms (56.78%) | Memory: 72.5 MB (82.69%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.js
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.js
@@ -1,25 +1,24 @@
-/**
+/** 00:49:47
  * @param {number[][]} points
  * @return {number}
  */
-var findMinArrowShots = function(points) {
+var findMinArrowShots = function (points) {
     let output = 0;
-    points.sort((a,b)=>a[1]-b[1])
+    points.sort((a, b) => a[1] - b[1])
     let prevS = points[0][0]
     let prevE = points[0][1]
 
-    for(let i=1; i<points.length; i++){
-        let [s,e] = points[i];
-        console.log(prevS, prevE)
-        if(prevS <= s) prevS = s;
-        if(e <= prevE) prevE = e;
+    for (let i = 1; i < points.length; i++) {
+        let [s, e] = points[i];
+        if (prevS <= s) prevS = s;
+        if (e <= prevE) prevE = e;
 
-        if(prevE < s){ 
+        if (prevE < s) {
             output++;
             prevS = s;
             prevE = e;
         }
     }
 
-    return output+1
+    return output + 1
 };
