d24e0c6f94a4a8e69090750bcee2b6e7f0560947
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-03-21 18:06:17 +0900

    Time: 157 ms (5.77%) | Memory: 58.4 MB (7.24%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxAreaOfIsland = function (grid) {
+    let output = 0;
+    let area = 0;
+    const m = grid.length;
+    const n = grid[0].length;
+
+    const findIsland = (row, col) => {
+        if (row < 0 || col < 0 ||
+            row >= m || col >= n ||
+            grid[row][col] === 0) return;
+
+        area += 1;
+        console.log(`grid[${row}][${col}]:${grid[row][col]} ${area}`)
+
+
+        grid[row][col] = 0;
+        
+        findIsland(row + 1, col); // down
+        findIsland(row - 1, col); // up
+        findIsland(row, col + 1); // right
+        findIsland(row, col - 1); // left
+        
+        return;
+    }
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            area = 0;
+            if (grid[i][j] === 1) {
+                findIsland(i, j, area);
+            }
+            output = Math.max(output, area)
+        }
+        console.log(`Max Area of Island : ${output}`)
+        console.log(`---------------`)
+    }
+
+    return output
+};
