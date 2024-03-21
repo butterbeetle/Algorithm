e8b21a7191a14074177988295381a0400ec95c63
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-03-21 15:31:31 +0900

    Time: 218 ms (39.13%) | Memory: 66.1 MB (69.57%) - LeetSync

---
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} rowSum
+ * @param {number[]} colSum
+ * @return {number[][]}
+ */
+var restoreMatrix = function (rowSum, colSum) {
+    const rowLength = rowSum.length;
+    const colLength = colSum.length;
+
+    const arr = Array.from({ length: rowLength }, () => new Array(colLength).fill(0))
+
+    for (let i = 0; i < rowLength; i++) {
+        for (let j = 0; j < colLength; j++) {
+            const value = Math.min(rowSum[i], colSum[j])
+            rowSum[i] -= value;
+            colSum[j] -= value;
+            arr[i][j] = value;
+        }
+    }
+
+    return arr
+};
+
+// 5 0 0
+// 3 4 0
+// 0 2 8
