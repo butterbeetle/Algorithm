fbc2b318c95d29349f5870f6a120261f0cf8c7b6
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-03-22 17:08:52 +0900

    Time: 59 ms (76.19%) | Memory: 52.6 MB (76.19%) - LeetSync

---
+++ 2508-maximum-sum-of-an-hourglass/maximum-sum-of-an-hourglass.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxSum = function (grid) {
+    let hourglass = 0;
+    const rows = grid.length
+    const cols = grid[0].length;
+
+    const getSize = (row, col) => {
+        return grid[row - 1][col - 1] + grid[row - 1][col] + grid[row - 1][col + 1]
+            + grid[row][col]
+            + grid[row + 1][col - 1] + grid[row + 1][col] + grid[row + 1][col + 1]
+    }
+
+    for (let i = 1; i < rows - 1; i++) {
+        for (let j = 1; j < cols - 1; j++) {
+            hourglass = Math.max(getSize(i, j), hourglass)
+        }
+    }
+
+    return hourglass
+};
