ce427fee4380de5c1b07707a9ef244e1cd80d100
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-03-22 17:49:09 +0900

    Time: 147 ms (23.38%) | Memory: 71.6 MB (5.12%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.js
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.js
@@ -11,7 +11,7 @@ var equalPairs = function (grid) {
     for (let i = 0; i < length; i++) {
         let rowStr = "";
         let colStr = "";
-        
+
         for (let j = 0; j < length; j++) {
             rowStr += grid[j][i] + "#"
             colStr += grid[i][j] + "#"
@@ -29,3 +29,37 @@ var equalPairs = function (grid) {
 
     return count
 };
+/*
+var equalPairs = function (grid) {
+    let count = 0;
+    const length = grid.length;
+    const rowsArr = []
+    const colsArr = []
+
+    for (let i = 0; i < length; i++) {
+        let rows = [];
+        let cols = [];
+        for (let j = 0; j < length; j++) {
+            rows.push(grid[j][i])
+            cols.push(grid[i][j])
+        }
+        rowsArr.push(rows)
+        colsArr.push(cols)
+    }
+
+    for (let i = 0; i < length; i++) {
+        for (let j = 0; j < length; j++) {
+            let isSame = true
+            for (let k = 0; k < length; k++) {
+                if (rowsArr[i][k] !== colsArr[j][k]) {
+                    isSame = false;
+                    break;
+                }
+            }
+            if (isSame) count++
+        }
+    }
+
+    return count
+};
+*/
