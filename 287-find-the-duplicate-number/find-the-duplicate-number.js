f767c5501717f9774237dd6e20b80c6acb279e2c
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-03-24 22:13:47 +0900

    Time: 55 ms (99.41%) | Memory: 60.7 MB (56.70%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.js
+++ 287-find-the-duplicate-number/find-the-duplicate-number.js
@@ -3,10 +3,17 @@
  * @return {number}
  */
 var findDuplicate = function (nums) {
-    const arr = new Array(nums.length).fill(false);
-
-    for (let i = 0; i < nums.length; i++) {
-        if (arr[nums[i]]) return nums[i]
-        arr[nums[i]] = true;
+    for (const num of nums) {
+        const index = Math.abs(num);
+        nums[index] *= -1;
+        if (nums[index] >= 0) {
+            return Math.abs(num);
+        }
     }
+    // const arr = new Array(nums.length).fill(false);
+
+    // for (let i = 0; i < nums.length; i++) {
+    //     if (arr[nums[i]]) return nums[i]
+    //     arr[nums[i]] = true;
+    // }
 };
