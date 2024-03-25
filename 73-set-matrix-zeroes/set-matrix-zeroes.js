e909f7bdeb96658701a1c896e1f16043349ee4a8
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-03-25 15:13:28 +0900

    Time: 73 ms (72.23%) | Memory: 54.1 MB (12.65%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.js
@@ -0,0 +1,40 @@
+/** 00:23:02
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function (matrix) {
+    const rows = matrix.length;
+    const cols = matrix[0].length;
+
+    console.log(rows, cols)
+
+    const makeZero = (row, col, direction) => {
+        if (row < 0 || col < 0 ||
+            row >= rows || col >= cols || matrix[row][col] === "Z") return;
+
+        matrix[row][col] = 0;
+
+        if (direction === "UP") makeZero(row - 1, col, direction);
+        else if (direction === "DOWN") makeZero(row + 1, col, direction);
+        else if (direction === "LEFT") makeZero(row, col - 1, direction);
+        else if (direction === "RIGHT") makeZero(row, col + 1, direction);
+    }
+
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            if (!matrix[row][col]) matrix[row][col] = "Z"
+        }
+    }
+
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            if (matrix[row][col] === "Z") {
+                makeZero(row - 1, col, "UP");
+                makeZero(row + 1, col, "DOWN");
+                makeZero(row, col - 1, "LEFT");
+                makeZero(row, col + 1, "RIGHT");
+                matrix[row][col] = 0;
+            }
+        }
+    }
+};
