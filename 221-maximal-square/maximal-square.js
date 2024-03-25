75bd66023d8513a1a7a3c62f24abf91430280acd
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-03-25 16:26:05 +0900

    Time: 68 ms (97.42%) | Memory: 57.3 MB (49.01%) - LeetSync

--- 221-maximal-square/maximal-square.js
+++ 221-maximal-square/maximal-square.js
@@ -10,18 +10,17 @@ var maximalSquare = function (matrix) {
     const arr = Array.from({ length: rows }, () => new Array(cols).fill(0));
 
     for (let col = 0; col < cols; col++) {
+        if(matrix[0][col] == 1) output = 1;
         arr[0][col] = +matrix[0][col]
-        output = Math.max(output, matrix[0][col])
     }
     for (let row = 0; row < rows; row++) {
+        if(matrix[row][0] == 1) output = 1;
         arr[row][0] = +matrix[row][0]
-        output = Math.max(output, matrix[row][0])
     }
-
+    
     for (let row = 1; row < rows; row++) {
         for (let col = 1; col < cols; col++) {
-            if (matrix[row][col] == 0) arr[row][col] = 0;
-            else {
+            if (matrix[row][col] == 1) {
                 arr[row][col] = +matrix[row][col]
                     + Math.min(arr[row - 1][col - 1], arr[row - 1][col], arr[row][col - 1])
                 output = Math.max(output, arr[row][col])
