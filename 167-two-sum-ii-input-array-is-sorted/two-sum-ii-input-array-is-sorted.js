99b58f07359f5e74646abddaae6d5c50a55c2ef1
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-03-29 16:31:44 +0900

    Time: 57 ms (51.88%) | Memory: 49.3 MB (97.04%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,15 @@
+/** 00:11:30
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+    let l = 0;
+    let r = numbers.length - 1;
+
+    while (l < r) {
+        if (numbers[l] + numbers[r] < target) l++;
+        else if (numbers[l] + numbers[r] > target) r--;
+        else return [l + 1, r + 1]
+    }
+};
