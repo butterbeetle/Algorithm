df5dd5f5415bfa3c4782de3076ced0cabff3cc83
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-04-02 17:47:34 +0900

    Time: 128 ms (44.07%) | Memory: 59.9 MB (71.19%) - LeetSync

--- 1094-matrix-cells-in-distance-order/matrix-cells-in-distance-order.js
+++ 1094-matrix-cells-in-distance-order/matrix-cells-in-distance-order.js
@@ -9,28 +9,35 @@ var allCellsDistOrder = function (rows, cols, rCenter, cCenter) {
     const output = [];
     const maxDis = Math.max(rCenter, rows - 1 - rCenter) + Math.max(cCenter, cols - 1 - cCenter);
     const tmp = [];
-    const grid = Array.from({ length: rows }, () => new Array(cols).fill(false));
+    // const grid = Array.from({ length: rows }, () => new Array(cols).fill(false));
 
-    for(let i=0; i<=maxDis; i++){
+    for (let i = 0; i <= maxDis; i++) {
         tmp.push([])
     }
-    
-    const jobs = (row, col) => {
-        if (row < 0 || col < 0 || row >= rows || col >= cols || grid[row][col]) return;
-        const dis = Math.abs(rCenter - row) + Math.abs(cCenter - col)
 
-        grid[row][col] = true;
-        tmp[dis].push([row,col])
-
-        jobs(row - 1, col)
-        jobs(row + 1, col)
-        jobs(row, col - 1)
-        jobs(row, col + 1)
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            const dis = Math.abs(rCenter - row) + Math.abs(cCenter - col)
+            tmp[dis].push([row, col])
+        }
     }
 
-    jobs(rCenter, cCenter)
+    // const jobs = (row, col) => {
+    //     if (row < 0 || col < 0 || row >= rows || col >= cols || grid[row][col]) return;
+    //     const dis = Math.abs(rCenter - row) + Math.abs(cCenter - col)
 
-    for(let i=0; i<=maxDis; i++){
+    //     grid[row][col] = true;
+    //     tmp[dis].push([row, col])
+
+    //     jobs(row - 1, col)
+    //     jobs(row + 1, col)
+    //     jobs(row, col - 1)
+    //     jobs(row, col + 1)
+    // }
+
+    // jobs(rCenter, cCenter)
+
+    for (let i = 0; i <= maxDis; i++) {
         output.push(...tmp[i])
     }
 
