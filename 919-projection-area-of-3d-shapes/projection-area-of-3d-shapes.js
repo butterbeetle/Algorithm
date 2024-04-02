ab287249917fb020cc1f52d31cebea664b56d244
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-04-02 16:13:32 +0900

    Time: 63 ms (22.92%) | Memory: 49.4 MB (83.33%) - LeetSync

---
+++ 919-projection-area-of-3d-shapes/projection-area-of-3d-shapes.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var projectionArea = function (grid) {
+    // 배열 요소 개수
+    // 열 최대값
+    // 행 최대값
+
+    const n = grid.length;
+
+    let a = 0;
+    let b = 0;
+    let c = 0;
+
+    for (let row = 0; row < n; row++) {
+        let bMax = 0;
+        for (let col = 0; col < n; col++) {
+            if (grid[row][col]) a += 1;
+            bMax = Math.max(bMax, grid[row][col])
+        }
+        b += bMax;
+
+        let cMax = 0;
+        for (let col = 0; col < n; col++) {
+            cMax = Math.max(cMax, grid[col][row])
+        }
+        c += cMax;
+    }
+
+    return a + b + c
+};
