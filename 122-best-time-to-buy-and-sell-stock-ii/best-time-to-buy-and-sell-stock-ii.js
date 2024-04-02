c35c1eae29f746c951c4926a9c2d3aa009e8d168
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-04-02 15:27:07 +0900

    Time: 64 ms (15.43%) | Memory: 48.9 MB (87.39%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
@@ -5,8 +5,6 @@
 var maxProfit = function (prices) {
     let profit = 0;
 
-    if (prices.length === 1) return 0;
-
     for (let i = 1; i < prices.length; i++) {
         if (prices[i] - prices[i - 1] > 0) profit += prices[i] - prices[i - 1]
     }
