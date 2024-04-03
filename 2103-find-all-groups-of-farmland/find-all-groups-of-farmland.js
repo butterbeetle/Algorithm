2057b20094f9d459e3dd47cbd1da92b74d18d83a
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-04-03 17:30:59 +0900

    Time: 209 ms (68.18%) | Memory: 74.1 MB (97.73%) - LeetSync

--- 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.js
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.js
@@ -6,22 +6,18 @@ var findFarmland = function (land) {
     const output = [];
     const rows = land.length;
     const cols = land[0].length;
+    let bRow = 0;
+    let bCol = 0;
 
-    const findGroup = (row, col, tmp) => {
+    const findGroup = (row, col) => {
         if (row < 0 || col < 0 || row >= rows || col >= cols || !land[row][col]) return;
         land[row][col] = 0;
 
-        tmp.push([row, col])
+        if (row > bRow) bRow = row;
+        if (col > bCol) bCol = col;
 
-        findGroup(row - 1, col, tmp);
-        findGroup(row + 1, col, tmp);
-        findGroup(row, col - 1, tmp);
-        findGroup(row, col + 1, tmp);
-
-        if (tmp.length > 1 && (tmp[tmp.length - 1][0] < row || tmp[tmp.length - 1][1] < col)) {
-            tmp.pop();
-        }
-
+        findGroup(row + 1, col);
+        findGroup(row, col + 1);
     }
 
     for (let row = 0; row < rows; row++) {
@@ -29,7 +25,9 @@ var findFarmland = function (land) {
             let tmp = [[row, col]];
             if (land[row][col]) {
                 findGroup(row, col, tmp)
-                output.push([tmp[0][0], tmp[0][1], tmp[tmp.length - 1][0], tmp[tmp.length - 1][1]])
+                output.push([row, col, bRow, bCol])
+                bRow = 0;
+                bCol = 0;
             }
         }
     }
