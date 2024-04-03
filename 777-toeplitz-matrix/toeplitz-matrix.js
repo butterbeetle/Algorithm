0eb897290b4bd7667cf62b2b4b83a0bda5dbb51b
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-04-03 16:33:25 +0900

    Time: 56 ms (74.56%) | Memory: 51 MB (64.38%) - LeetSync

---
+++ 777-toeplitz-matrix/toeplitz-matrix.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} matrix
+ * @return {boolean}
+ */
+var isToeplitzMatrix = function (matrix) {
+    const rows = matrix.length;
+    const cols = matrix[0].length;
+
+    for (let row = 1; row < rows; row++) {
+        for (let col = 1; col < cols; col++) {
+            if (matrix[row][col] !== matrix[row - 1][col - 1]) return false
+        }
+    }
+
+    return true
+};
