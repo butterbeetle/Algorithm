170efdd1e9a1bae0847269c1c39915eb4936e93a
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-04-08 17:58:15 +0900

    Time: 158 ms (5.21%) | Memory: 60.2 MB (5.21%) - LeetSync

---
+++ 566-reshape-the-matrix/reshape-the-matrix.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[][]} mat
+ * @param {number} r
+ * @param {number} c
+ * @return {number[][]}
+ */
+var matrixReshape = function (mat, r, c) {
+    const rows = mat.length;
+    const cols = mat[0].length;
+
+    if (rows * cols !== r * c) return mat;
+
+    let sr = 0;
+    let sc = 0;
+
+    const grid = Array.from({ length: r }, () => new Array(c).fill(0));
+
+    for (let row = 0; row < r; row++) {
+        for (let col = 0; col < c; col++) {
+            console.log(`mat[${sr}][${sc}] -> grid[${row}][${col}]`)
+            grid[row][col] = mat[sr][sc]
+            sc++;
+            if (sc === cols) {
+                sr++;
+                sc = 0;
+            }
+        }
+    }
+
+    console.log(grid)
+
+    return grid
+};
