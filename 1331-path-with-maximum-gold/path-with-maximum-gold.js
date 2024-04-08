f58a037aaf2bb2fe107e139839ce3a7a572dcf29
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-04-08 17:32:49 +0900

    Time: 263 ms (29.58%) | Memory: 52.8 MB (21.13%) - LeetSync

---
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.js
@@ -0,0 +1,32 @@
+/** 00:18:50
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var getMaximumGold = function (grid) {
+    let output = 0;
+    const rows = grid.length;
+    const cols = grid[0].length;
+
+    const mining = (row, col) => {
+        if (row < 0 || col < 0 || row >= rows || col >= cols || !grid[row][col]) return 0;
+
+        const tmp = grid[row][col];
+
+        grid[row][col] = 0;
+
+        const max = Math.max(mining(row - 1, col), mining(row + 1, col), mining(row, col - 1), mining(row, col + 1));
+
+        grid[row][col] = tmp;
+
+        return tmp + max
+    }
+
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            const tmp = grid.map(v => [...v]);
+            if (grid[row][col]) output = Math.max(output, mining(row, col))
+        }
+    }
+
+    return output
+};
