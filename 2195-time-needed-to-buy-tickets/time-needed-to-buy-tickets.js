0ef76f58a00b2ba551d129609f22730e4890d248
butterbeetle <50831567+butterbeetle@users.noreply.github.com>
2024-04-09 11:35:31 +0900

    Time: 52 ms (69.40%) | Memory: 48.6 MB (87.43%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.js
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.js
@@ -5,16 +5,21 @@
  */
 var timeRequiredToBuy = function (tickets, k) {
     let output = 0;
-
-    while (tickets[k]) {
-        for (let i = 0; i < tickets.length; i++) {
-            if (tickets[i]) {
-                output++;
-                tickets[i]--;
-            }
-            if (!tickets[k]) break;
-        }
+    
+    for (let i = 0; i < tickets.length; i++) {
+        if (i <= k) output += Math.min(tickets[i], tickets[k])
+        else output += Math.min(tickets[i], tickets[k]-1)
     }
+
+    // while (tickets[k]) {
+    //     for (let i = 0; i < tickets.length; i++) {
+    //         if (tickets[i]) {
+    //             output++;
+    //             tickets[i]--;
+    //         }
+    //         if (!tickets[k]) break;
+    //     }
+    // }
 
     return output
 };
